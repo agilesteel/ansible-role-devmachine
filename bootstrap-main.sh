14b7369de8caf3aecb060893cc7140758045aa2a
Vladyslav Pekker <agilesteel@gmail.com>
2016-03-24 07:35:17 +0100

    Fixed bootstrap-main.sh.

--- bootstrap-main.sh
+++ bootstrap-main.sh
@@ -2,12 +2,15 @@
 
 # install ansible
 sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get install ansible -y
+sudo apt-get install -y ansible
 
 # download main.yml
-wget -q https://raw.githubusercontent.com/agilesteel/ansible-role-devmachine/master/main.yml
+wget -q -O main.yml https://raw.githubusercontent.com/agilesteel/ansible-role-devmachine/master/main.yml
 
 # setup devmachine
 ansible-playbook -K main.yml
+
+# cleanup
+rm main.yml
