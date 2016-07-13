cd503b68e441745bcbb2b747a25be0561cd96c6a
Vladyslav Pekker <agilesteel@gmail.com>
2016-07-13 15:15:12 +0200

    Renames.

--- bootstrap-provision.sh
+++ bootstrap-provision.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 # download main.yml
-wget -q -O main.yml https://raw.githubusercontent.com/agilesteel/ansible-role-devmachine/master/main.yml
+wget -q -O main.yml https://raw.githubusercontent.com/agilesteel/ansible-role-devmachine/master/provision.yml
 wget -q -O mongodb.service https://raw.githubusercontent.com/agilesteel/ansible-role-devmachine/master/mongodb.service
 
 # install ansible
 sudo apt-get install -yqq software-properties-common
-sudo apt-add-repository -yqq ppa:ansible/ansible
+sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -yqq ansible
 
 # setup devmachine
-sudo ansible-playbook main.yml
+sudo ansible-playbook provision.yml
 
 # cleanup
 rm mongodb.service
-rm main.yml
-rm bootstrap-main.sh
+rm provision.yml
+rm bootstrap-provision.sh
