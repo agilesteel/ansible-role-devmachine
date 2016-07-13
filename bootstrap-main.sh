5da68172c6838685859e0a6b8906b85816357ba7
Vladyslav Pekker <agilesteel@gmail.com>
2016-07-13 12:04:58 +0200

    Added -qq.

--- bootstrap-main.sh
+++ bootstrap-main.sh
@@ -5,10 +5,10 @@ wget -q -O main.yml https://raw.githubusercontent.com/agilesteel/ansible-role-de
 wget -q -O mongodb.service https://raw.githubusercontent.com/agilesteel/ansible-role-devmachine/master/mongodb.service
 
 # install ansible
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get install -yqq software-properties-common
+sudo apt-add-repository -yqq ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get install -y ansible
+sudo apt-get install -yqq ansible
 
 # setup devmachine
 sudo ansible-playbook main.yml
